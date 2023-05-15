8a1bbe14209358b316198fadef0f191297e2b83e
willhwiltjr <108745261+willhwiltjr@users.noreply.github.com>
2023-05-14 18:17:19 -0700

    refactored WordsDictionary should be more streamlined

--- src/main/java/wiltwi7062/evil_hangman/WordsDictionary.java
+++ src/main/java/wiltwi7062/evil_hangman/WordsDictionary.java
@@ -4,67 +4,57 @@
  */
 package wiltwi7062.evil_hangman;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.TreeMap;
-import java.util.Vector;
 
 /**
  *
  * @author wiltw
+ * @param <<error>>
  */
-public class WordsDictionary<K, V> extends TreeMap {
-    private Vector<K> keys;
-    private Vector<V> values;
+public class WordsDictionary extends TreeMap {
+    private final String keys;
+    private final Family values;
     
     public WordsDictionary() {
-        keys = new Vector<K>();
-        values = new Vector<V>();
+        keys = "";
+        values = new Family();
     }
     
-    public V add(K key, V value) {
+    public ArrayList add(String key, String value) {
         if (keys.contains(key)) {
             int location = keys.indexOf(key);
-            V temp = values.get(location);
-            values.set(location, value);
+            ArrayList<String> temp = (ArrayList)values.getEntry(location);
+            temp.add(value);
             return temp;
         } else {
-            keys.add(key);
-            values.add(value);
+            ArrayList<String> temp = new ArrayList<>();
+            temp.add(value);
+            put(key,temp);
             return null;
         }
     }
     
-    public V Remove(K key) {      
+    public String Remove(String key) {      
         if (keys.contains(key)) {
             int location = keys.indexOf(key);
-            V temp = values.get(location);
             values.remove(location);
-            keys.remove(location);
-            return temp;
+            return key;
         } else {
             return null;
         }        
     }
     
-    public V getValue(K key) {      
-        if (keys.contains(key)) {
-            int location = keys.indexOf(key);
-            V temp = values.get(location);
-            return temp;
-        } else {
-            return null;
-        }               
-    } 
+    
     
     public boolean contains(K key) {
         return keys.contains(key);               
     }  
     
-    public Iterator<K> getKeyIterator() {
-        return keys.iterator();        
-    }
     
-    public Iterator<V> getValueIterator() {    
+    
+    public Iterator getValueIterator() {    
         return values.iterator();          
     } 
     
@@ -73,14 +63,7 @@ public class WordsDictionary<K, V> extends TreeMap {
         return keys.isEmpty();        
     } 
     
-    public int getSize() {  
-        return keys.size();
-    }
     
-    @Override
-    public void clear() { 
-        keys.clear();
-        values.clear();
-    }
+    
     
 }
